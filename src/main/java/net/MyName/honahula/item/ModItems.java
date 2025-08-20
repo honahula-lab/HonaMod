1f28e6d8df9151b66278feb01c9694ae97296c87
honahula-lab <skyler.honahula@gmail.com>
2025-08-20 17:32:55 -0400

    good

---
+++ src/main/java/net/MyName/honahula/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.MyName.honahula.item;
+
+import net.MyName.honahula.honahula;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Objects;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, honahula.Mod_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+
+    }
+}
